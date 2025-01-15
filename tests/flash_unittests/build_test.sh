d13e95fa41ed83c45a7c106f992e3735c9c56a1b
SimpleTheoryOfTypes <jiading.gai@gmail.com>
2025-01-14 23:05:02 -0800

    [flash attention] unittest for flash attention components.

---
+++ tests/flash_unittests/build_test.sh
@@ -0,0 +1,3 @@
+# TODO: use cmake to drive all the tests
+# try cuda 11.7 if has any problem compiling
+/usr/local/cuda-11.7/bin/nvcc tiled_copy.cu -I../../cutlass/include I../../cutlass/tools/util/include --expt-relaxed-constexpr
